56fa0863f69e1b1b8aff6fbac190a43ba52606b3
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-13 16:48:39 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,31 @@
 def using_push(array, string)
   array.push(string)
 end
+
+def using_unshift(array, string)
+
+end
+
+def using_pop(array)
+  
+end
+
+def pop_with_args(array)
+  
+end
+
+def using_shift(array)
+  
+end
+
+def shift_with_args(array)
+  
+end
+
+def using_concat(array1, array2)
+  
+end
+
+def using_insert(array, element)
+  
+end
